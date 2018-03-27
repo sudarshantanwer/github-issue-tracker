4f82fc0266d44f0427dbc67354f1922a696a5614
sudarshan <sudarshan@tothenew.com>
2018-03-28 05:07:57 +0530

    Folder structure added

---
+++ templates/javascript/app.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name <%= scriptAppName %>
+ * @description
+ * # <%= scriptAppName %>
+ *
+ * Main module of the application.
+ */
+angular
+  .module('<%= scriptAppName %>', [<%= angularModules %>])<% if (ngRoute) { %>
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.hashPrefix('');
+  })<% } %>;
